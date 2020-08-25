5e785f2a652288d6949c04b9c428d6f6106afed8
Ali GOREN <goren.ali@yandex.com>
2020-08-25 22:26:07 +0300

    Settings added

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -20,6 +20,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
     content = models.TextField(null=True)
     slug = models.SlugField(max_length=100, null=True, unique=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # when global comment setting is on, this field will be available
+    comments_enabled = models.BooleanField(default=True)
     
     category = models.ManyToManyField(Category)
 
@@ -29,6 +35,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(unidecode.unidecode(self.title))
+        
         return super().save(*args, **kwargs)
 
 
@@ -36,6 +43,12 @@ class Page(models.Model):
     title = models.CharField(max_length=200, null=True)
     content = models.TextField(null=True)
     slug = models.SlugField(max_length=100, null=True, unique=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # when global comment setting is on, this field will be available
+    comments_enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
@@ -44,3 +57,17 @@ class Page(models.Model):
         if not self.slug:
             self.slug = slugify(unidecode.unidecode(self.title))
         return super().save(*args, **kwargs)
+
+
+class Setting(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    
+    posts_per_page = models.IntegerField(default=10)
+
+    # this overrides all post and page comment options
+    comments_enabled = models.BooleanField(default=True)
+
+    comments_needs_approval = models.BooleanField(default=False)
+
+    post_permalink = models.CharField(max_length=100, default="%postname%")
