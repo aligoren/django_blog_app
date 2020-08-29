08eb9cc6ded78a1f93876947fa8c0a424da55d60
Ali GOREN <goren.ali@yandex.com>
2020-08-29 12:45:35 +0300

    Pagination added

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Setting
 
 def index(request):
 
-    posts = Post.objects.all().order_by('-id')
+    post_list = Post.objects.all().order_by('-id')
     settings = Setting.objects.first()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(post_list, settings.posts_per_page)
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     context = { 'posts': posts, 'settings': settings }
 
