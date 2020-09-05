bf1d188fc6d7e3776238c5af50833cff3188b70d
Ali GOREN <goren.ali@yandex.com>
2020-09-05 22:15:29 +0300

    All, Published and Draft post features added with search feature

--- blog/blog_app/controllers/dashboard_posts_view.py
+++ blog/blog_app/controllers/dashboard_posts_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.db.models import Q
 
 from ..models import Post, Setting
 from ..forms import PostForm
@@ -15,10 +16,26 @@ class DashboardPostsView(LoginRequiredMixin, TemplateView):
 
     def get(self, request):
 
-        post_list = Post.objects.filter(active=True).order_by('-id')
+        post_status = request.GET.get('status', 'published')
+        page = request.GET.get('page', 1)
+        search = request.GET.get('search', '')
+        active = True
+
+        if post_status == 'published':
+            active = True
+        elif post_status == 'draft':
+            active = False
+        elif post_status == 'all':
+            active = None
+
+        if search:
+            post_list = Post.objects.filter(Q(active=active) & (Q(title__contains=search) | Q(content__contains=search))).order_by('-id')
+        else:
+            post_list = Post.objects.filter(active=active).order_by('-id') if active is not None else Post.objects.all()
+        
         settings = Setting.objects.first()
 
-        page = request.GET.get('page', 1)
+        
 
         paginator = Paginator(post_list, settings.posts_per_page)
 
